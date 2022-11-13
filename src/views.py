9585950212fc6fc33391a443504937640d9fac76
012xx <night.fox.045@gmail.com>
2022-11-13 16:30:42 +0000

    :sparkles: : read csv

--- src/views.py
+++ src/views.py
@@ -1,5 +1,20 @@
 from src import app
+from csv import reader
+
+csvPath = "./data/data.csv"
+
+# open file
+with open(csvPath,"r") as searchTarget:
+  # pass the file object reader()
+  file_reader = reader(searchTarget)
+  # do this for all the rows
+  for i in file_reader:
+    # print the rows
+    print(i)
 
 @app.route("/")
 def index():
   return "Hello Flask"
+
+
+
