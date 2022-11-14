06e9992805d2db5b61b0c27db63129682215990b
012xx <night.fox.045@gmail.com>
2022-11-14 01:15:48 +0000

    :sparkles: : add templates

--- src/views.py
+++ src/views.py
@@ -1,20 +1,25 @@
-from src import app
+from flask import render_template
 from csv import reader
+from src import app
 
 csvPath = "./data/data.csv"
 
 # open file
-with open(csvPath,"r") as searchTarget:
-  # pass the file object reader()
-  file_reader = reader(searchTarget)
-  # do this for all the rows
-  for i in file_reader:
-    # print the rows
-    print(i)
+# with open(csvPath,"r") as searchTarget:
+#   # pass the file object reader()
+#   reader = reader(searchTarget)
+#   # do this for all the rows
+#   for i in reader:
+#     # print the rows
+#     print(i)
 
 @app.route("/")
 def index():
-  return "Hello Flask"
+  return render_template('index.html')
+
+# debug code
+if __name__=='views':
+    app.run(debug=True)
 
 
 
