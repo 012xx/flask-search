0b31e21db986c33093625441c82cb5750d596e72
012xx <night.fox.045@gmail.com>
2023-01-08 15:48:43 +0000

    Add: query取得

--- main/views.py
+++ main/views.py
@@ -4,30 +4,37 @@ from main import app
 import pandas as pd
 csvPath = "./data/data.csv"
 
-
-@app.route("/all",methods=['GET', 'POST'])
+@app.route("/",methods=['GET'])
 def index():
+  # データを取得する
+  df = pd.read_csv(csvPath)
+  data_lists = df.values.tolist()
+
+  # queryがある場合には上のデータからqueryでfilterかけてデータを削除する
+  searchWord = request.args.get('search', '')
+  if searchWord:
+  for i, data in enumerate(data_lists):
+    # もしdata要素にsearchWordがなければ、i番目の要素を削除する
+
+  return render_template('index.html', searchWord=searchWord, data_lists=data_lists)
+
+# CSVファイルの全データを出力するpath
+@app.route("/all",methods=['GET'])
+def all():
   # CSVファイルをPandasでロード
   df = pd.read_csv(csvPath)
 
   # データフレームをリストに変換してテンプレートに渡す
   return render_template('index.html',data_lists=df.values.tolist())
 
+# 検索するときのpath
 @app.route("/search",methods=['GET', 'POST'])
 def search():
   if request.method == 'GET':
-    return render_template('search.html')
-  elif request.method == 'POST':
     searchWord = request.form.get("searchWord")
     return render_template('search.html', searchWord = searchWord)
-
-    # with open("./data/data.csv","r") as CSVfile:
-      # reader = reader(CSVfile)
-      # result = []
-      # for row in reader:
-      #   if poststr is row:
-      #     result.append(row)
-      #     return render_template('search.html', result = result)
+  elif request.method == 'POST':
+    return render_template('search.html')
 
 
 if __name__ == "__main__":
